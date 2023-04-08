4d7c241914a216082868d239ff68ec35d4ae3098
Rabin Poudyal <rabinpoudyal1995@gmail.com>
2023-04-08 15:33:14 +1000

    add elasticsearch to products model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
   validates :name, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :gtin, presence: true
